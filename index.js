91ac147cf230929ea8fd0ad6d32c28d259677a2b
Rumen Rumenov <mmetalonlyy@gmail.com>
2016-10-07 16:53:37 +0300

    better validation error reporting

--- index.js
+++ index.js
@@ -263,6 +263,9 @@ class Core {
 						return;
 					}
 					CORE.logger.error(req.locals.error)
+					if (req.locals.error.message.indexOf('Validation error') !== -1) {
+						req.locals.error.customMessage = 'Validation error - please make sure all required fields are present and in the correct format.'
+					}
 					res.status(req.locals.errorStatus).json({error: req.locals.error.customMessage || 'An internal server error has occurred. Please try again.'})
 				})
 
@@ -347,6 +350,9 @@ class Core {
 						return;
 					}
 					CORE.logger.error(req.locals.error)
+					if (req.locals.error.message.indexOf('Validation error') !== -1) {
+						req.locals.error.customMessage = 'Validation error - please make sure all required fields are present and in the correct format.'
+					}
 					res.status(req.locals.errorStatus).json({error: req.locals.error.customMessage || 'An internal server error has occurred. Please try again.'})
 				})
 
