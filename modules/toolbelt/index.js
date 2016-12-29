67f5c119643da2ec9cacfc6e1922698745d92d3d
Rumen Rumenov <mmetalonlyy@gmail.com>
2016-12-29 12:15:22 +0200

    getnested in the ramster toolbelt

--- modules/toolbelt/index.js
+++ modules/toolbelt/index.js
@@ -1,26 +1,41 @@
 'use strict'
 
 let emptyToNull = (data, outputData) => {
-	for (let key in data) {
-		let currentValue = null
+		for (let key in data) {
+			let currentValue = null
 
-		if (data[key] instanceof Array) {
-			currentValue = emptyToNull(data[key], [])
-		} else if (typeof data[key] === 'object') {
-			currentValue = emptyToNull(data[key], {})
-		} else if (data[key] === '') {
-			currentValue = null
-		} else {
-			currentValue = data[key]
-		}
+			if (data[key] instanceof Array) {
+				currentValue = emptyToNull(data[key], [])
+			} else if (typeof data[key] === 'object') {
+				currentValue = emptyToNull(data[key], {})
+			} else if (data[key] === '') {
+				currentValue = null
+			} else {
+				currentValue = data[key]
+			}
 
-		if (outputData instanceof Array) {
-			outputData.push(currentValue)
-			continue;
+			if (outputData instanceof Array) {
+				outputData.push(currentValue)
+				continue;
+			}
+			outputData[key] = currentValue
 		}
-		outputData[key] = currentValue
+		return outputData
+	},
+	getNested = ({parent, field}) => {
+		if ((typeof parent !== 'object') || (typeof field !== 'string')) {
+			return null
+		}
+		let fieldData = field.split('.'),
+			currentElement = parent
+		for (let i in fieldData) {
+			let innerElement = fieldData[i]
+			if (!currentElement || !currentElement[innerElement]) {
+				return currentElement
+			}
+			currentElement = currentElement[innerElement]
+		}
+		return currentElement
 	}
-	return outputData
-}
 
-module.exports = {emptyToNull}
+module.exports = {emptyToNull, getNested}
